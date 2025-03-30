1c4cadc49eb9585af4e97b9cf790152534969182
githubkrunal <githubkrunal@gmail.com>
2025-03-30 17:21:57 +0530

    first commit

---
+++ watchmate/watchlist_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from watchlist_app.views import movie_list,movie_detail
+
+urlpatterns = [
+    path("list/",movie_list,name='movie-list'),  # Include watchlist_app URLs
+    path("<int:movie_id>/",movie_detail,name="movie-detail"),  # Include watchlist_app URLs
+]
---
+++ watchmate/watchlist_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from watchlist_app.models import Movie
+from django.http import JsonResponse
+
+def movie_list(request):
+    movies=Movie.objects.all()
+    print(list(movies.values()))
+    data ={
+        'movies':list(movies.values())
+    }
+    return JsonResponse(data)
+
+def movie_detail(request, movie_id):
+    movie=Movie.objects.get(pk=movie_id)
+    data={
+        "name": movie.name,
+        "description": movie.description,
+        "active": movie.active
+    }
+    print(movie)
+    return JsonResponse(data)
