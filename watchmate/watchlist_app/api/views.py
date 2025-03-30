dd62dc6acca897821b4e5519a62452f69910f3fb
githubkrunal <githubkrunal@gmail.com>
2025-03-30 19:56:39 +0530

    api with views

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,12 +1,15 @@
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
+@api_view(['GET'])  # Allow only GET requests
 def movie_list(request):
     movies=Movie.objects.all()
-    serializer=MovieSerializer(movies)
+    serializer=MovieSerializer(movies,many=True)
     return Response(serializer.data)
 
+@api_view(['GET'])  # Allow only GET requests
 def movie_detail(request, movie_id):
     movie=Movie.objects.get(pk=movie_id)
     serializer=MovieSerializer(movie)
