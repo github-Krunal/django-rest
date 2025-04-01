61ec7d88c7ce998648d280f3a1b4d5da05675cd3
githubkrunal <githubkrunal@gmail.com>
2025-04-01 08:39:36 +0530

    CRUD operations

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -3,16 +3,42 @@ from rest_framework.decorators import api_view
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
-@api_view(['GET'])  # Allow only GET requests
+@api_view(['GET','post'])  # Allow only GET requests
 def movie_list(request):
-    movies=Movie.objects.all()
-    serializer=MovieSerializer(movies,many=True)
-    return Response(serializer.data)
+    if request.method=='GET':
+        movies=Movie.objects.all()
+        serializer=MovieSerializer(movies,many=True)
+        return Response(serializer.data)
+    
+    if request.method=='POST':  
+        serializer=MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        else:
+            return Response(serializer.errors)
 
-@api_view(['GET'])  # Allow only GET requests
+@api_view(['GET','PUT','DELETE'])  # Allow only GET requests
 def movie_detail(request, movie_id):
-    movie=Movie.objects.get(pk=movie_id)
-    serializer=MovieSerializer(movie)
-    return Response(serializer.data) 
+    if request.method=='GET':  # Retrieve a movie
+        movie=Movie.objects.get(pk=movie_id)
+        serializer=MovieSerializer(movie)
+        return Response(serializer.data) 
+
+    if request.method=='PUT':
+        movie=Movie.objects.get(pk=movie_id)
+        serializer=MovieSerializer(movie,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+            
+    if request.method=='DELETE':
+        movie=Movie.objects.get(pk=movie_id)
+        movie.delete()
+        return Response(status=204)  # No content status code
+        
+     
     
     
