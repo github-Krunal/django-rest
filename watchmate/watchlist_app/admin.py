56c1317ae5b6c34ad32c01b36f8aed7f2b1aab8b
githubkrunal <githubkrunal@gmail.com>
2025-04-11 08:54:36 +0530

    review model added

--- watchmate/watchlist_app/admin.py
+++ watchmate/watchlist_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from watchlist_app.models import WatchList,SteramPlatform
+from watchlist_app.models import WatchList,SteramPlatform,Review
 # Register your models here.
 admin.site.register(WatchList)
 admin.site.register(SteramPlatform)
+admin.site.register(Review)
